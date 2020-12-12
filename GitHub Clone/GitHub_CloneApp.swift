4e0c5b012f525c4db2cde6edad52e047590fdd38
“KrisReid” <krisreid_06@hotmail.com>
2020-12-12 13:10:04 +0000

    Initial Commit

---
+++ GitHub Clone/GitHub_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_CloneApp.swift
+//  GitHub Clone
+//
+//  Created by Kris Reid on 12/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
